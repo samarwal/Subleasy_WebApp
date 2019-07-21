a0247c18b6a2293a7c46e5de77c627cb8c9b8fc8
sam_agarwal <agarwalsamarth06@gmail.com>
2019-07-21 17:50:43 -0400

    final touches

--- js/search.js
+++ js/search.js
@@ -2,17 +2,17 @@ function redirTo() {
     var city = document.getElementById("city").value;
     var duration = document.getElementById("duration").value;
 
-    if (city == "toronto") {
+    if(city=="none" || duration=="0"){
+        alert("please select from the dropdown menu");
+    } else if (city == "toronto") {
         if (duration == "4") {
-            location.assign("/fourAll.html");
+            location.assign("fourAll.html");
         } else if (duration == "8") {
-            location.assign("/eightAll.html");
+            location.assign("eightAll.html");
         } else if (duration == "12") {
-            location.assign("/twelveAll.html");
-        } else {
-            location.assign("allToronto.html");
+            location.assign("twelveAll.html");
         }
     } else {
-        location.assign("/noMatch.html");
+        location.assign("noMatch.html");
     }
 }
